0e53a4286ae874fc2e8df0041997bd090ae71dc9
Ivan <ashikhin1998@gmail.com>
2023-04-22 14:45:51 +0600

    #2 Удаление
    Реализовал фунцию удаление фотографии

--- photo/urls.py
+++ photo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from photo.views import IndexView, AddPhotoView, PhotoDetail, PhotoUpdateView
+from photo.views import IndexView, AddPhotoView, PhotoDetail, PhotoUpdateView, PhotoDeleteView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('add_post/', AddPhotoView.as_view(), name='add_photo'),
     path('photo/<int:pk>', PhotoDetail.as_view(), name='details'),
     path('photo/<int:pk>/update', PhotoUpdateView.as_view(), name='photo_update'),
+    path('photo/<int:pk>/delete', PhotoDeleteView.as_view(), name='delete_photo'),
 ]
--- photo/views.py
+++ photo/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.urls import reverse
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from photo.forms import SearchForm, PhotoForm
 from photo.models import Photo
 
@@ -70,3 +70,10 @@ class PhotoDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class PhotoDeleteView(DeleteView):
+    template_name = 'confirm_delete.html'
+    context_object_name = 'photo'
+    model = Photo
+    success_url = reverse_lazy('index')
